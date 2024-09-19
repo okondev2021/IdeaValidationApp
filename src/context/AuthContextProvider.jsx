6f38a9e86b94c83e9d78c6c82f75bba90bac0473
okondev2021 <sojapril16@gmail.com>
2024-09-19 17:05:30 +0100

    added refresh token function

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,4 +1,4 @@
-import {useContext, createContext, useState } from "react"
+import {createContext, useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
@@ -16,15 +16,24 @@ const AuthContextProvider = ({children}) => {
         localStorage.getItem("AuthToken") ? jwtDecode(JSON.parse(localStorage.getItem("AuthToken")).refresh).full_name : null
     )
 
+
+    const [authDataObject, setAuthDataObject] = useState(
+        localStorage.getItem("AuthToken") ? JSON.parse(localStorage.getItem("AuthToken")) : null
+    )
+
+
     const handleAuthSuccess = (data, refresh) => {
         localStorage.setItem("AuthToken",JSON.stringify(data))
         setLoggedInUser(jwtDecode(refresh).full_name)
+        setAuthDataObject(data)
         navigate("/")
     }
 
 
     const userRegistration = async (e) => {
+
         e.preventDefault();
+
         const full_name = e.target.full_name.value
         const email = e.target.email.value
         const password = e.target.password.value
@@ -49,6 +58,7 @@ const AuthContextProvider = ({children}) => {
 
 
     const userLogin = async (e) => {
+
         e.preventDefault();
 
         const email = e.target.email.value
@@ -71,12 +81,13 @@ const AuthContextProvider = ({children}) => {
                   throw new Error('Incorrect credentials. Please try again.');
                 } 
                 else {
-                    // Custom general message for other errors
+                // Custom general message for other errors
                   throw new Error('An error occurred. Please try again later.');
                 }
             }
             else{
                 handleAuthSuccess(data, data.refresh)
+                console.log(jwtDecode(data.access))
             }
         }
         catch(error){
@@ -87,15 +98,14 @@ const AuthContextProvider = ({children}) => {
 
     const userLogout = async () => {
 
-        const authDataObject = JSON.parse(localStorage.getItem("AuthToken"))
         let response = await fetch('http://127.0.0.1:8000/user/logout/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authDataObject.access}`
+                'Authorization': `Bearer ${authDataObject?.access}`
             },
             body: JSON.stringify({
-                refresh:authDataObject.refresh
+                refresh:authDataObject?.refresh
             })
         })
 
@@ -104,23 +114,81 @@ const AuthContextProvider = ({children}) => {
         if(response.ok){
             localStorage.removeItem("AuthToken")
             setLoggedInUser(null)
+            setAuthDataObject(null)
+        }
+        else{
+            navigate('/signin')
         }
     }
 
 
+    const refreshToken = async () => {
+
+        let response = await fetch('http://127.0.0.1:8000/user/refresh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                refresh:authDataObject?.refresh
+            })
+        })
+
+        let data = await response.json()
+
+        if (response.ok){
+            localStorage.setItem("AuthToken",JSON.stringify(data))
+            setLoggedInUser(jwtDecode(data.refresh).full_name)
+            setAuthDataObject(data)
+            console.log(jwtDecode(data.access))
+        }
+        else{
+            console.log("This refresh token is no longer valid")
+        }
+    }
+
+
+    const renewAuthToken = (token) => {
+
+        if (!token)return;
+    
+        const decodedToken = jwtDecode(token.access);
+        const currentTime = Math.floor(Date.now() / 1000);
+        let timeStampMinutes = (decodedToken.exp - currentTime - 60) * 1000
+
+        setTimeout(() => {
+            refreshToken();
+        }, timeStampMinutes);
+
+
+    };
+
+
+    useEffect(() => {
+        // Set the refresh token timer when the component mounts or authDataObject state changes
+        renewAuthToken(authDataObject);
+    }, [authDataObject]);
+
+    
     let contextData = {
         LoggedinUser: loggedInUser,
+        setLoggedInUser : setLoggedInUser,
+        setAuthDataObject: setAuthDataObject,
         userRegistration: userRegistration,
         userLogin: userLogin,
         userLogout: userLogout,
     }
 
-
+    
     return(
         <AuthInfoContext.Provider value = {contextData} >
             {children}
         </AuthInfoContext.Provider>
     )
+
+
 }
+
   
 export default AuthContextProvider
+
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,23 @@
+import { useContext, useRef } from "react"
+import { jwtDecode } from "jwt-decode";
 import { AuthInfoContext } from "../context/AuthContextProvider"
-import { useContext } from "react"
 
 const MainPage = () => {
 
-    const {LoggedinUser, userLogout} = useContext(AuthInfoContext)
+    const {LoggedinUser, userLogout, setLoggedInUser, setAuthDataObject} = useContext(AuthInfoContext)
+
+    const isAuthTokenPresent = useRef(JSON.parse(localStorage.getItem("AuthToken")));
+
+    if (isAuthTokenPresent.current){
+      const decodedToken = jwtDecode(isAuthTokenPresent.current?.refresh)
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (currentTime > decodedToken.exp){
+        // reset everything since this user is no longer authenticated (refresh token has expired)
+        localStorage.removeItem("AuthToken")
+        setLoggedInUser(null)
+        setAuthDataObject(null)
+      }
+    }
 
     return (
         <div>
